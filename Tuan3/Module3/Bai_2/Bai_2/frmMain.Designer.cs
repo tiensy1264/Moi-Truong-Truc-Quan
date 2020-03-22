732199aae93e20b190dc55d2bcbaba3aaa742e7a
tiensy <tiensy1264@gmail.com>
2020-03-22 18:09:01 +0700

    done

---
+++ Tuan3/Module3/Bai_2/Bai_2/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Bai_2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblChieuDai = new System.Windows.Forms.Label();
+            this.lblChieuRong = new System.Windows.Forms.Label();
+            this.txtChieuDai = new System.Windows.Forms.TextBox();
+            this.txtChieuRong = new System.Windows.Forms.TextBox();
+            this.txtDienTich = new System.Windows.Forms.TextBox();
+            this.lblDienTich = new System.Windows.Forms.Label();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnTiep = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblChieuDai
+            // 
+            this.lblChieuDai.AutoSize = true;
+            this.lblChieuDai.Location = new System.Drawing.Point(133, 43);
+            this.lblChieuDai.Name = "lblChieuDai";
+            this.lblChieuDai.Size = new System.Drawing.Size(53, 13);
+            this.lblChieuDai.TabIndex = 0;
+            this.lblChieuDai.Text = "Chiều Dài";
+            // 
+            // lblChieuRong
+            // 
+            this.lblChieuRong.AutoSize = true;
+            this.lblChieuRong.Location = new System.Drawing.Point(133, 90);
+            this.lblChieuRong.Name = "lblChieuRong";
+            this.lblChieuRong.Size = new System.Drawing.Size(63, 13);
+            this.lblChieuRong.TabIndex = 1;
+            this.lblChieuRong.Text = "Chiều Rộng";
+            // 
+            // txtChieuDai
+            // 
+            this.txtChieuDai.Location = new System.Drawing.Point(199, 40);
+            this.txtChieuDai.Name = "txtChieuDai";
+            this.txtChieuDai.Size = new System.Drawing.Size(100, 20);
+            this.txtChieuDai.TabIndex = 2;
+            this.txtChieuDai.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChieuDai_KeyPress);
+            // 
+            // txtChieuRong
+            // 
+            this.txtChieuRong.Location = new System.Drawing.Point(199, 87);
+            this.txtChieuRong.Name = "txtChieuRong";
+            this.txtChieuRong.Size = new System.Drawing.Size(100, 20);
+            this.txtChieuRong.TabIndex = 3;
+            this.txtChieuRong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChieuRong_KeyPress);
+            // 
+            // txtDienTich
+            // 
+            this.txtDienTich.Location = new System.Drawing.Point(199, 134);
+            this.txtDienTich.Name = "txtDienTich";
+            this.txtDienTich.ReadOnly = true;
+            this.txtDienTich.Size = new System.Drawing.Size(100, 20);
+            this.txtDienTich.TabIndex = 4;
+            // 
+            // lblDienTich
+            // 
+            this.lblDienTich.AutoSize = true;
+            this.lblDienTich.Location = new System.Drawing.Point(133, 137);
+            this.lblDienTich.Name = "lblDienTich";
+            this.lblDienTich.Size = new System.Drawing.Size(51, 13);
+            this.lblDienTich.TabIndex = 5;
+            this.lblDienTich.Text = "Diện tích";
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(78, 190);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTinh.TabIndex = 6;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // btnTiep
+            // 
+            this.btnTiep.Location = new System.Drawing.Point(179, 190);
+            this.btnTiep.Name = "btnTiep";
+            this.btnTiep.Size = new System.Drawing.Size(75, 23);
+            this.btnTiep.TabIndex = 7;
+            this.btnTiep.Text = "Tiếp";
+            this.btnTiep.UseVisualStyleBackColor = true;
+            this.btnTiep.Click += new System.EventHandler(this.btnTiep_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(278, 190);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnTinh;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnThoat;
+            this.ClientSize = new System.Drawing.Size(433, 255);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTiep);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.lblDienTich);
+            this.Controls.Add(this.txtDienTich);
+            this.Controls.Add(this.txtChieuRong);
+            this.Controls.Add(this.txtChieuDai);
+            this.Controls.Add(this.lblChieuRong);
+            this.Controls.Add(this.lblChieuDai);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chương trình tính diện tích hình chữ nhật";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblChieuDai;
+        private System.Windows.Forms.Label lblChieuRong;
+        private System.Windows.Forms.TextBox txtChieuDai;
+        private System.Windows.Forms.TextBox txtChieuRong;
+        private System.Windows.Forms.TextBox txtDienTich;
+        private System.Windows.Forms.Label lblDienTich;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Button btnTiep;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Tuan3/Module3/Bai_2/Bai_2/frmMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            double chieuDai = 0, chieuRong = 0, dienTich;
+            if (txtChieuDai.Text == "")
+            {
+                MessageBox.Show("Nhập Chiều Dài!");
+                txtChieuDai.SelectAll();
+                txtChieuDai.Focus();
+            }
+            else if (txtChieuRong.Text == "")
+            {
+                MessageBox.Show("Nhập Chiều Rộng!");
+                txtChieuRong.SelectAll();
+                txtChieuRong.Focus();
+            }
+            else
+            {
+                chieuDai = Double.Parse(txtChieuDai.Text.Trim(), CultureInfo.InvariantCulture);
+                chieuRong = Double.Parse(txtChieuRong.Text.Trim(), CultureInfo.InvariantCulture);
+
+                if (chieuDai <= 0)
+                {
+                    MessageBox.Show("Chiều Dài > 0!");
+                    txtChieuDai.SelectAll();
+                    txtChieuDai.Focus();
+                }
+                else if (chieuRong <= 0)
+                {
+                    MessageBox.Show("Chiều Rộng > 0!");
+                    txtChieuRong.SelectAll();
+                    txtChieuRong.Focus();
+                }
+                else
+                {
+                    dienTich = chieuDai * chieuRong;
+                    //dienTich = 2.1 * 3;
+                    txtDienTich.Text = dienTich.ToString();
+                }
+            }
+        }
+
+        private void btnTiep_Click(object sender, EventArgs e)
+        {
+            txtChieuDai.Clear();
+            txtChieuRong.Clear();
+            txtDienTich.Clear();
+            txtChieuDai.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dl;
+            dl = MessageBox.Show("Có chắc chắn là bạn muốn đóng ứng dụng không?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dl == DialogResult.Yes) Close();
+        }
+
+        private void txtChieuDai_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                // Allow Digits
+            }
+            else if (e.KeyChar == '.' && !((TextBox)sender).Text.Contains('.'))
+            {
+                //Allows only one Dot Char
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            /*if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }*/
+        }
+
+        private void txtChieuRong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 'A' && e.KeyChar <= 'Z') || (e.KeyChar >= 'a' && e.KeyChar <= 'z')) e.Handled = true;
+        }
+    }
+}
